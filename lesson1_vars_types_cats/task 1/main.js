9cea833a4ac340db67c1fcab5def40e14f0109be
Illia-debug <dazle1994@gmail.com>
2025-02-05 17:34:41 +0300

    lesson1_vars_types_cats

---
+++ lesson1_vars_types_cats/task 1/main.js
@@ -0,0 +1,30 @@
+// #dYQNrBV
+// Створити змінні. Присвоїти кожному з них значення:
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.
+let word1 = 'hello';
+let word2 = 'owu';
+let word3 = 'com';
+let word4 = 'ua';
+let number1 = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+let number5 = 3.14;
+let number6 = 2.7;
+let number7 = 16;
+let boolean1 = true;
+let boolean2 = false;
+console.log(word1)
+console.log(word2)
+console.log(word3)
+console.log(word4)
+console.log(number1)
+console.log(number2)
+console.log(number3)
+console.log(number4)
+console.log(number5)
+console.log(number6)
+console.log(number7)
+console.log(boolean1)
+console.log(boolean2)
---
+++ lesson1_vars_types_cats/task2/main.js
@@ -0,0 +1,8 @@
+// #6Qb97gsv
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+//     З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+let firstName = 'Illia';
+let middleName = 'Zagunailo';
+let lastName = 'Olegovich';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
